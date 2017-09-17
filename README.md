bb8aeedb2fa22c12c7ba188e393a96604b5b0bce
stevenlii <stevenlii@qq.com>
2017-09-17 21:17:44 +0800

    update redis for myprop

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # basic-cache-redis
 redis simple
-呈现redis simple工具使用
+单机使用redis (使用jedis)
+现redis simple工具使用
+RedisUtilSimple 和 RedisUtil配合jedis的使用为，使得redis单机使用效率提升
---
+++ src/main/java/com/paymoon/basic/commons/core/test.java
@@ -0,0 +1,22 @@
+package com.paymoon.basic.commons.core;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public final class test {
+	private static JedisPool pool;
+	private static Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+    	String testString = MyProp.getVariable("redis.password");
+    	String testString2 = MyProp.getVariable("redis.port");
+//    	System.out.println(testString);
+//    	System.out.println(testString2);
+    	
+	}
+    
+}
